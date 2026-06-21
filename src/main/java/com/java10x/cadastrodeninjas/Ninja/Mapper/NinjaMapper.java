64a174128dc21b5f80744d4a451825583ef56111
WendellTEC <wendelltecno@gmail.com>
2026-06-21 15:24:31 -0300

    chore: Included the version of DTO map manually at NinjaMapper

--- src/main/java/com/java10x/cadastrodeninjas/Ninja/Mapper/NinjaMapper.java
+++ src/main/java/com/java10x/cadastrodeninjas/Ninja/Mapper/NinjaMapper.java
@@ -1,4 +1,38 @@
 package com.java10x.cadastrodeninjas.Ninja.Mapper;
 
+import com.java10x.cadastrodeninjas.Ninja.DTO.NinjaDTO;
+import com.java10x.cadastrodeninjas.Ninja.Model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map (NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissions(ninjaDTO.getMissions());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissions(ninjaModel.getMissions());
+
+        return ninjaDTO;
+    }
+
 }
